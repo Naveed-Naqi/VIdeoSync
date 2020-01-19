e19ce85e7a5fa9d702fcb5ed0195f455be5c93d5
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-18 22:16:31 -0500

    started using react-player to create a video player on the frontend that streams a video from the server, which can be accessed throug hthe video endpoint

--- client/src/components/dashboard/DashboardContainer.js
+++ client/src/components/dashboard/DashboardContainer.js
@@ -2,12 +2,52 @@ import React, { Component } from "react";
 import { logoutUser } from "../../actions/authActions";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import ReactPlayer from "react-player";
 
 class DashboardContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: 0,
+      isPlaying: false
+    };
+  }
+
+  handleProgress = state => {
+    console.log(this.state);
+    this.setState(state);
+  };
+
+  handlePause = () => {
+    this.setState({
+      isPlaying: false
+    });
+  };
+
+  handlePlay = () => {
+    this.setState({
+      isPlaying: true
+    });
+  };
+
   render() {
+    let { loaded, isPlaying } = this.state;
     return (
       <div>
         <h1>You are logged in.</h1>
+
+        <ReactPlayer
+          controls
+          playing={isPlaying}
+          onPause={this.handlePause}
+          onPlay={this.handlePlay}
+          onProgress={this.handleProgress}
+          url="http://localhost:1234/api/users/video"
+        ></ReactPlayer>
+
+        <h1>Loading Progress</h1>
+        <progress max={1} value={loaded} />
 
         <div>
           <button onClick={this.props.logoutUser}>Logout</button>
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { User } = require("../database/models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
 
 const validateRegisterInput = require("../validation/register");
 const validateLoginInput = require("../validation/login");
@@ -99,6 +101,45 @@ router.post("/login", (req, res, next) => {
       }
     });
   });
+});
+
+router.get("/video", function(req, res) {
+  const path = "assets/sample.mp4";
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    if (start >= fileSize) {
+      res
+        .status(416)
+        .send("Requested range not satisfiable\n" + start + " >= " + fileSize);
+      return;
+    }
+
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4"
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
 });
 
 module.exports = router;
